6a6e8fbd52523289bcb3e3ee04435c1dea90ae51
Ngo Thanh Thai <thanhthaiqngai3011@gmail.com>
2020-06-08 08:12:52 +0700

    Them ghi chu

---
+++ Lab456_NgoThanhThai/Migrations/202006010243063_PopulateCategoryTable.cs
@@ -0,0 +1,19 @@
+namespace Lab456_NgoThanhThai.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateCategoryTable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES(1, 'Deverlopment')");
+            Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES(2, 'Business')");
+            Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES(3, 'Marketing')");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- Lab456_NgoThanhThai/Models/ApplicationUser.cs
+++ Lab456_NgoThanhThai/Models/ApplicationUser.cs
@@ -15,19 +15,9 @@ namespace Lab456_NgoThanhThai.Models
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             // Add custom user claims here
             return userIdentity;
+            //them dong ghi chu
         }
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
-        }
-
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
-        }
-    }
+    
 }
