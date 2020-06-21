73c9b29e7fec94dcd4069d4bbb7196ab1c08cd7d
thaidevhutech <thanhthaiqngai3011@gmail.com>
2020-06-21 21:05:40 +0700

    change

--- Lab456_NgoThanhThai/Global.asax.cs
+++ Lab456_NgoThanhThai/Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Lab456_NgoThanhThai.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,6 +18,8 @@ namespace Lab456_NgoThanhThai
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Database.SetInitializer<ApplicationDbContext>(null);
+
         }
     }
 }
